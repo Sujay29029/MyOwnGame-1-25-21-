18f8de39853098ede9dd3abbd945686465b2e3da
Sujay29029 <70997037+Sujay29029@users.noreply.github.com>
2021-01-25 09:48:21 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# MyOwnGame-1-25-21-
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,24 @@
+var database;
+var gameState = 0;
+var playerCount = 0;
+var form, player, game;
+var back_img;
+var allPlayers;
+
+function setup(){
+  createCanvas(displayWidth - 20, displayHeight - 100);
+
+  back_img = loadImage("Images/Background Image.jpg");
+  
+  database = firebase.database();
+  
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw() {
+  background(back_img);
+  drawSprites();
+}
